9eeb1c78742393aacea09b9644d185fd42901b78
FlatCult <ivlad6543@gmail.com>
2025-02-15 21:16:49 +0500

    Homework #10

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,48 @@
-function factorial(x) {
-    return x < 0 ? console.error("Факториал отрицательного числа не определён") : (x == 0 || x == 1) ? 1 : x * factorial(x - 1);
-}
+// UTILS FUNCTION
 
-const user = { name: "John", age: 30 };
-
-function userInfo(user) {
-    if (!user) return "Обьект не передан";
-    if (Object.keys(user).length < 1) return "Передан пустой объект";
-    if (!user.age) return "В объекте не передан ключ 'age'";
-    return user;
-}
-
-let city1 = {};
-city1.name = "ГородN", city1.population = 10000000;
-
-city1.getName = function() {
-    return this.name;
-};
-
-city1.exportStr = function() {
-    return "name=" + this.name + ", population=" + this.population;
-};
-
-const city2 = {
-    name: "ГородM",
-    population: 10000000000,
-    getName: function() {
-        return this.name;
+const utils = {
+    sp: (int) => {
+        int = int.toString();
+        if(int >= 10000) {
+            return int.split('').reverse().join('').match(/[0-9]{1,3}/g).join('.').split('').reverse().join(''); 
+        } else {
+            return int
+        }
     },
-    exportStr: function() {
-        return "name=" + this.name + ", population=" + this.population;
+    ssp: (int) => { 
+        int = int.toString();
+        if(int >= 10000) {
+            return int.split('').reverse().join('').match(/[0-9]{1,3}/g).join(' ').split('').reverse().join(''); 
+        } else {
+            return int
+        }
     }
-};
+}
+
+class Car_base {
+    constructor(weight, color, speed, mark, model, carClass, year, price, hp) {
+        this._weight = weight;
+        this._color = color;
+        this._speed = speed;
+        this._mark = mark;
+        this._model = model;
+        this._carClass = carClass;
+        this._year = year;
+        this._price = price;
+        this._hp = hp;
+    }
+
+    kilometers(value) {
+        if (value <= 0 || typeof value !== "number") return console.error(`Для правильной работы функции необходимо передать число > 0.`);
+        return `За ${value} часов машина проедет ${Math.floor(this._speed * value)} километров`;
+    }
+}
+
+const cars = [new Car_base(1575, "жёлтый", 350, "Lamborghini", "Aventador", "S", "2016 - 2022", "41 994 000 - 74 505 668₽", 770), new Car_base(1970, "чёрный", 400, "Bugatti", "Chiron", "S", "2016 - 2023", "390 000 000 - 420 000 000₽", 1500), new Car_base(2580, "чёрный", 230, "Mercedes-Benz", "G65 AMG", "G", "2015 - 2018", "7 640 000₽", 630)];
+
+for (let i = 0; i < cars.length; i++) {
+    console.log(`ИНФОРМАЦИЯ ОБ АВТОМОБИЛЕ ${cars[i]._mark.toUpperCase()} ${cars[i]._model.toUpperCase()}:\n\nМарка: ${cars[i]._mark}\nМодель: ${cars[i]._model}\nМаксимальная скорость: ${cars[i]._speed}км/ч\nМощность: ${cars[i]._hp} л.с.\nКласс машины: ${cars[i]._carClass}-класс\nМасса автомобиля: ${cars[i]._weight} кг.\nЦвет автомобиля: ${cars[i]._color}\nГода выпуска: ${cars[i]._year}\nЦена автомобиля: ${cars[i]._price}\n${cars[i].kilometers(12)}\n\n-----------------------------------------------------\n\n`);
+}
 
 function pickFunction() {
     return alert("Пока отключено");
