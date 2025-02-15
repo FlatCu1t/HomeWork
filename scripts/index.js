314b58522c3032aaec0440981edcdfb1b0872150
FlatCult <ivlad6543@gmail.com>
2025-02-15 21:26:02 +0500

    Homework #10 исправление.

--- scripts/index.js
+++ scripts/index.js
@@ -21,27 +21,27 @@ const utils = {
 
 class Car_base {
     constructor(weight, color, speed, mark, model, carClass, year, price, hp) {
-        this._weight = weight;
-        this._color = color;
-        this._speed = speed;
-        this._mark = mark;
-        this._model = model;
-        this._carClass = carClass;
-        this._year = year;
-        this._price = price;
-        this._hp = hp;
+        this.weight = weight;
+        this.color = color;
+        this.speed = speed;
+        this.mark = mark;
+        this.model = model;
+        this.carClass = carClass;
+        this.year = year;
+        this.price = price;
+        this.hp = hp;
     }
 
     kilometers(value) {
         if (value <= 0 || typeof value !== "number") return console.error(`Для правильной работы функции необходимо передать число > 0.`);
-        return `За ${value} часов машина проедет ${Math.floor(this._speed * value)} километров`;
+        return `За ${value} часов машина проедет ${Math.floor(this.speed * value)} километров`;
     }
 }
 
 const cars = [new Car_base(1575, "жёлтый", 350, "Lamborghini", "Aventador", "S", "2016 - 2022", "41 994 000 - 74 505 668₽", 770), new Car_base(1970, "чёрный", 400, "Bugatti", "Chiron", "S", "2016 - 2023", "390 000 000 - 420 000 000₽", 1500), new Car_base(2580, "чёрный", 230, "Mercedes-Benz", "G65 AMG", "G", "2015 - 2018", "7 640 000₽", 630)];
 
 for (let i = 0; i < cars.length; i++) {
-    console.log(`ИНФОРМАЦИЯ ОБ АВТОМОБИЛЕ ${cars[i]._mark.toUpperCase()} ${cars[i]._model.toUpperCase()}:\n\nМарка: ${cars[i]._mark}\nМодель: ${cars[i]._model}\nМаксимальная скорость: ${cars[i]._speed}км/ч\nМощность: ${cars[i]._hp} л.с.\nКласс машины: ${cars[i]._carClass}-класс\nМасса автомобиля: ${cars[i]._weight} кг.\nЦвет автомобиля: ${cars[i]._color}\nГода выпуска: ${cars[i]._year}\nЦена автомобиля: ${cars[i]._price}\n${cars[i].kilometers(12)}\n\n-----------------------------------------------------\n\n`);
+    console.log(`ИНФОРМАЦИЯ ОБ АВТОМОБИЛЕ ${cars[i].mark.toUpperCase()} ${cars[i].model.toUpperCase()}:\n\nМарка: ${cars[i].mark}\nМодель: ${cars[i].model}\nМаксимальная скорость: ${cars[i].speed}км/ч\nМощность: ${cars[i].hp} л.с.\nКласс машины: ${cars[i].carClass}-класс\nМасса автомобиля: ${cars[i].weight} кг.\nЦвет автомобиля: ${cars[i].color}\nГода выпуска: ${cars[i].year}\nЦена автомобиля: ${cars[i].price}\n${cars[i].kilometers(12)}\n\n-----------------------------------------------------\n\n`);
 }
 
 function pickFunction() {
