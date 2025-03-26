c1621bb47b1b60a1761663f65d259cd7044cf9e2
FlatCult <ivlad6543@gmail.com>
2025-03-26 21:34:52 +0500

    23_Создание игры на JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,7 @@ function getUnix() {
     return Math.floor(new Date().getTime());
 }
 
-function unixStamp(stamp) {
+function unixStamp(stamp, type) {
     let date = new Date(stamp),
     year = date.getFullYear(),
     month = (date.getMonth() + 1).toString().padStart(2, "0"),
@@ -73,65 +73,173 @@ function unixStampDays(stamp, stamp2) {
     return { seconds: s, minutes: m, hours: h, days: d, years: years, text: text };
 }
 
-document.getElementById("myForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
-    const name = document.getElementById("name").value.trim();
-    const phone = document.getElementById("phone").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value;
-    const age = document.getElementById("age").value.trim();
+canvas.width = 900;
+canvas.height = 600;
 
-    let valid = true;
-    let errors = [];
+let game;
+const box = 20;
+let snake = [];
+snake[0] = { x: 9 * box, y: 10 * box };
+let direction = "RIGHT";
+let changingDirection = false;
+let score = 0;
+let gameTime = 0;
+let gameTimer;
 
-    const nameRegex = /^влад$/i;
-    if (!nameRegex.test(name)) {
-        valid = false;
-        errors.push("Имя должно быть 'Влад'.");
+const img = new Image();
+img.src = "./images/food.svg";
+const settings = { headColor: "lime", tailColor: "white" };
+
+function calculateGameTime(gameTime) {
+    const minutes = Math.floor(gameTime / 60);
+    const seconds = gameTime % 60;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutes}:${formattedSeconds}`;
+}
+
+function generateFood() {
+    let newFood;
+    let collision;
+    do {
+        collision = false;
+        newFood = {
+            x: Math.floor(Math.random() * (canvas.width / box)) * box,
+            y: Math.floor(Math.random() * (canvas.height / box)) * box
+        };
+
+        for (let i = 0; i < snake.length; i++) {
+        if (newFood.x === snake[i].x && newFood.y === snake[i].y) {
+                collision = true;
+                break;
+            }
+        }
+    } while (collision);
+    return newFood;
+}
+  
+let food = generateFood();
+
+function control(event) {
+    if (changingDirection) return;
+    const key = event.keyCode;
+    if (key == 37 && direction !== "RIGHT") {
+        direction = "LEFT";
+        changingDirection = true;
+    } else if (key == 38 && direction !== "DOWN") {
+        direction = "UP";
+        changingDirection = true;
+    } else if (key == 39 && direction !== "LEFT") {
+        direction = "RIGHT";
+        changingDirection = true;
+    } else if (key == 40 && direction !== "UP") {
+        direction = "DOWN";
+        changingDirection = true;
     }
+}
 
-    const cleanedPhone = phone.replace(/[()\-\+]/g, "");
-    if (!/^\d+$/.test(cleanedPhone)) {
-        valid = false;
-        errors.push("Телефон может содержать только цифры, скобки, тире и плюс.");
-    }
+document.addEventListener("keydown", control);
 
-    const emailRegex = /^(?=.{8,156}$)(?=.*\d)(?=.*@)(?=.*\.)/;
-    if (!emailRegex.test(email)) {
-        valid = false;
-        errors.push("Почта должна быть от 8 до 156 символов и содержать @, точку и цифру.");
-    }
-
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,12}$/;
-    if (!passwordRegex.test(password)) {
-        valid = false;
-        errors.push("Пароль должен быть от 6 до 12 символов и содержать одну маленькую, одну большую букву и цифру.");
-    }
-
-    if (!/^\d+$/.test(age)) {
-        valid = false;
-        errors.push("Возраст должен содержать только цифры.");
-    } else {
-        const ageNum = parseInt(age, 10);
-        if (ageNum < 10 || ageNum > 120) {
-        valid = false;
-        errors.push("Возраст должен быть в диапазоне от 10 до 120.");
+function collision(head, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (head.x === array[i].x && head.y === array[i].y) {
+            return true;
         }
     }
 
-    if (!valid) {
-        alert("Ошибки: \n" + errors.join("\n"));
+    return false;
+}
+
+function draw() {
+    changingDirection = false;
+    
+    ctx.fillStyle = "rgb(44, 44, 44)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = i === 0 ? settings.headColor : settings.tailColor;
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.drawImage(img, food.x, food.y, 25, 25);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction === "LEFT") snakeX -= box;
+    if (direction === "RIGHT") snakeX += box;
+    if (direction === "UP") snakeY -= box;
+    if (direction === "DOWN") snakeY += box;
+
+    if (snakeX === food.x && snakeY === food.y) {
+        food = generateFood();
+        score++;
+        document.getElementById("game_score").textContent = `Счёт: ${score}`;
+    } else {
+        snake.pop();
+    }
+
+    let newHead = { x: snakeX, y: snakeY };
+
+    if (snakeX < 0 || snakeX >= canvas.width || snakeY < 0 || snakeY >= canvas.height || collision(newHead, snake))
+    {
+        clearInterval(game);
+        clearInterval(gameTimer);
+        document.getElementById("game_Title").textContent = "ЗМЕЙКА (GAME OVER)"
         return;
     }
 
-    const formData = {
-        name: name,
-        phone: cleanedPhone,
-        email: email,
-        password: password,
-        age: age
-    };
+    snake.unshift(newHead);
+}
 
-    return console.log("НОВАЯ АВТОРИЗАЦИЯ:", formData);
+game = setInterval(draw, 110);
+gameTimer = setInterval(() => {
+    gameTime++;
+    document.getElementById("game_time").textContent = `Время игры: ${calculateGameTime(gameTime)}`;
+}, 1000);
+
+function restartGame() {
+    snake = [];
+    snake[0] = { x: 9 * box, y: 10 * box };
+    direction = "RIGHT";
+    changingDirection = false;
+    score = 0;
+    gameTime = 0;
+    document.getElementById("game_Title").textContent = "ЗМЕЙКА";
+    document.getElementById("game_score").textContent = "Счёт: 0";
+    document.getElementById("game_time").textContent = "Время игры: 0:00";
+    clearInterval(game);
+    clearInterval(gameTimer);
+    food = generateFood();
+    game = setInterval(draw, 110);
+    gameTimer = setInterval(() => {
+        gameTime++;
+        document.getElementById("game_time").textContent = `Время игры: ${calculateGameTime(gameTime)}`;
+    }, 1000);
+};
+
+document.getElementById("restartButton").addEventListener("click", () => {
+    return restartGame();
+});
+
+function applySettings() {
+    const inputs = document.querySelectorAll(".settings_input");
+    inputs.forEach((el, index) => {
+        switch (index) {
+            case 0:
+                settings.headColor = el.value;
+                break;
+            case 1:
+                settings.tailColor = el.value;
+                break;
+        }
+    });
+}
+
+document.getElementById("settings_submit").addEventListener("click", () => {
+    return applySettings();
 });
