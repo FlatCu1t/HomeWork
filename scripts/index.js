8f8f6f076b77d54d828183176c63fc16d03a9616
FlatCult <ivlad6543@gmail.com>
2025-04-07 22:08:02 +0500

    32_Асинхронность. Промисы. Async-Await. Fetch

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,7 @@ const utils = {
 
 function getUnix() {
     return Math.floor(new Date().getTime());
-}
+};
 
 function unixStamp(stamp) {
     let date = new Date(stamp),
@@ -73,7 +73,6 @@ function unixStampDays(stamp, stamp2) {
     return { seconds: s, minutes: m, hours: h, days: d, years: years, text: text };
 }
 
-<<<<<<< HEAD
 // Задание 1
 
 async function asyncAdd(a, b) {
@@ -214,108 +213,3 @@ async function loadUserCards() {
 }
 
 loadUserCards();
-=======
-const WEATHER_API_KEY = "5247757012a3493eb1a142844250704";
-
-// Input с городом
-const cityInput = document.querySelector(".cityInput");
-
-// Картинка погоды
-const weatherImage = document.querySelector(".weather_img");
-
-// Сегодняшний день
-const currentDayWeek = document.querySelector(".currentDayWeek");
-const currentDay = document.querySelector(".currentDay");
-
-// Текущие градусы
-const degrees = document.querySelector(".degrees");
-const weatherType = document.querySelector(".weather_type");
-
-// Ветер
-const windSpeed = document.querySelector(".speed");
-const windType = document.querySelector(".speed_second");
-const SpeedContainer = document.querySelector(".speed_container");
-
-// Шанс дождя
-const rainChance = document.querySelector(".chanceRain");
-
-// Давление
-const mbar = document.querySelector(".mbar");
-
-// Влажность
-const humidityChance = document.querySelector(".percent");
-const secondHumidityChance = document.querySelector(".humidityPercent");
-
-function currentDayFunc() {
-    const today = new Date();
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const months = ["Dec", "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov"]
-    const dayName = daysOfWeek[today.getDay()];
-    const monthName = months[(today.getMonth() + 1)];
-    currentDayWeek.textContent = dayName;
-    currentDay.textContent = `${monthName} ${today.getDate().toString().padStart(2, "0")}`;
-}
-
-currentDayFunc();
-
-function getWindDescription(windSpeed) {
-    switch (true) {
-        case (windSpeed < 1):
-            return "Calm";
-        case (windSpeed >= 1 && windSpeed < 15):
-            return "Light breeze";
-        case (windSpeed >= 15 && windSpeed < 30):
-            return "Moderate breeze";
-        case (windSpeed >= 30 && windSpeed < 50):
-            return "Strong wind";
-        case (windSpeed >= 50 && windSpeed < 75):
-            return "Storm";
-        default:
-            return "Hurricane";
-    }
-}
-
-async function getWeatherData(location) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${location}&aqi=no`);
-    if (response.ok && response.status == 200) {
-        const data = await response.json();
-        return data;
-    }
-}
-
-cityInput.addEventListener("input", async function() {
-    const city = cityInput.value.trim();
-
-    if (city) {
-        const weatherData = await getWeatherData(city);
-        degrees.textContent = Math.floor(weatherData.current.temp_c);
-        weatherType.textContent = weatherData.current.condition.text;
-        windSpeed.textContent = weatherData.current.wind_mph + " km/h";
-        windType.textContent = getWindDescription(weatherData.current.wind_mph);
-        SpeedContainer.style.right = (windType.textContent.length > 4 && windType.textContent.length <= 12) ? "-15px" : windType.textContent.length > 12 ? "-45px" : "0";
-        rainChance.textContent = `${weatherData.current.precip_mm}%`
-        mbar.textContent = `${weatherData.current.pressure_mb} mbar`;
-        humidityChance.textContent = `${weatherData.current.humidity}%`;
-        secondHumidityChance.textContent = `Humidity ${weatherData.current.humidity}%`;
-        weatherImage.src = weatherData.current.condition.icon;
-    }
-});
-
-async function defaultData() {
-    const weatherData = await getWeatherData("Astana");
-    degrees.textContent = Math.floor(weatherData.current.temp_c);
-    weatherType.textContent = weatherData.current.condition.text;
-    windSpeed.textContent = weatherData.current.wind_mph + " km/h";
-    windType.textContent = getWindDescription(weatherData.current.wind_mph);
-    SpeedContainer.style.right = (windType.textContent.length > 4 && windType.textContent.length <= 12) ? "-15px" : windType.textContent.length > 12 ? "-45px" : "0";
-    rainChance.textContent = `${weatherData.current.precip_mm}%`
-    mbar.textContent = `${weatherData.current.pressure_mb} mbar`;
-    humidityChance.textContent = `${weatherData.current.humidity}%`;
-    secondHumidityChance.textContent = `Humidity ${weatherData.current.humidity}%`;
-    weatherImage.src = weatherData.current.condition.icon;
-}
-
-(async () => {
-    await defaultData();
-})();
->>>>>>> 825c18195df7a24e21932d6d2f19c5fd3ae09c87
