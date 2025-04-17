f86d8be4e3f92cd6a52fe3140779a03cad630d5a
FlatCult <ivlad6543@gmail.com>
2025-04-17 13:17:16 +0500

    29_Настройка объектов. Модули. Генераторы

--- scripts/index.js
+++ scripts/index.js
@@ -1,108 +1,132 @@
-// UTILS FUNCTION
-const utils = {
-    sp: (int) => {
-        int = int.toString();
-        if (int >= 10000) {
-            return int.split('').reverse().join('').match(/[0-9]{1,3}/g).join('.').split('').reverse().join('');
-        } else {
-            return int;
-        }
-    },
-    ssp: (int) => { 
-        int = int.toString();
-        if (int >= 10000) {
-            return int.split('').reverse().join('').match(/[0-9]{1,3}/g).join(' ').split('').reverse().join('');
-        } else {
-            return int;
-        }
-    },
-    decl: (n, titles) => { return titles[(n % 10 === 1 && n % 100 !== 11) ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2] },
-    rand: (min, max) => { return Math.floor(Math.random() * (max - min + 1)) + min }
-}
+import { Functions } from "./functions.js";
+const functions = new Functions();
 
-function getUnix() {
-    return Math.floor(new Date().getTime());
+document.getElementById('search-btn').addEventListener('click', async function() {
+    const title = document.getElementById('movie-title').value.trim();
+    if (!title) return;
+
+    const data = await functions.getFilmsData(title);
+    const container = document.getElementById('movie-container');
+    container.innerHTML = '';
+
+    if (data.Response === "True") {
+        data.Search.sort(function(a, b) { 
+            if (b.Year > a.Year) return 1 
+            if (b.Year < a.Year) return -1 
+            return 0
+        });
+
+        data.Search.forEach(movie => {
+            const poster = (movie.Poster && movie.Poster !== "N/A") ? movie.Poster : "./images/no-logo.png";
+            const movieHTML = `
+                <div class="movie">
+                <img src="${poster}" alt="Постер фильма">
+                <div class="movie-details">
+                    <h2>${movie.Title}</h2>
+                    <p><strong>Год:</strong> ${movie.Year}</p>
+                </div>
+                </div>
+            `;
+            container.innerHTML += movieHTML;
+        });
+    } else {
+        container.innerHTML = '<p>Фильм не найден</p>';
+    }
+});
+
+const now = new Date();
+
+const dateStore = {
+    day: now.getDate(),
+    month: now.getMonth() + 1,
+    year: now.getFullYear()
 };
 
-function unixStamp(stamp) {
-    let date = new Date(stamp),
-    year = date.getFullYear(),
-    month = (date.getMonth() + 1).toString().padStart(2, "0"),
-    day = date.getDate().toString().padStart(2, "0"),
-    hour = date.getHours().toString().padStart(2, "0"),
-    minutes = date.getMinutes().toString().padStart(2, "0"),
-    secs = date.getSeconds().toString().padStart(2, "0");
+const Person = {};
+Object.defineProperties(Person, {
+    firstName: {
+        value: "Иван",
+        writable: true,
+        enumerable: true,
+        configurable: false
+    },
+    lastName: {
+        value: "Иван",
+        writable: true,
+        enumerable: true,
+        configurable: false
+    },
 
-    return { text: `${day}.${month}.${year}, ${hour}:${minutes}:${secs}`, y: year, m: month, d: day, h: hour, m: minutes, s: secs };
-}
+    day: {
+        enumerable: true,
+        configurable: false,
+        get() {
+            return dateStore.day;
+        },
+        set(val) {
+            const num = Number(val);
+            if (!Number.isInteger(num) || num < 1 || num > 31) {
+                throw new Error('Неверный день: должно быть целое от 1 до 31');
+            }
+            dateStore.day = num;
+        }
+    },
 
-function unixStampDays(stamp, stamp2) {
-    let date1 = new Date(stamp);
-    let date2 = new Date(stamp2);
+    month: {
+        enumerable: true,
+        configurable: false,
+        get() {
+            return dateStore.month;
+        },
+        set(val) {
+            const num = Number(val);
+            if (!Number.isInteger(num) || num < 1 || num > 12) {
+                throw new Error('Неверный месяц: должно быть целое от 1 до 12');
+            }
+            dateStore.month = num;
+        }
+    },
 
-    let years = date1.getFullYear() - date2.getFullYear();
-    let lastAnniversary = new Date(date2);
-    lastAnniversary.setFullYear(date2.getFullYear() + years);
-    if (lastAnniversary > date1) {
-        years--;
-        lastAnniversary.setFullYear(date2.getFullYear() + years);
+    year: {
+        enumerable: true,
+        configurable: false,
+        get() {
+            return dateStore.year;
+        },
+        set(val) {
+            const num = Number(val);
+            if (!Number.isInteger(num) || num < 1900) {
+                throw new Error('Неверный год: должно быть целое ≥ 1900');
+            }
+            dateStore.year = num;
+        }
     }
-    
-    let remainderMs = date1 - lastAnniversary;
-    
-    let s = Math.floor(remainderMs / 1000) % 60;
-    let m = Math.floor(remainderMs / (1000 * 60)) % 60;
-    let h = Math.floor(remainderMs / (1000 * 60 * 60)) % 24;
-    let d = Math.floor(remainderMs / (1000 * 60 * 60 * 24));
+});
 
-    let text = "";
-    if (years > 0) {
-        text += `${years} ${utils.decl(years, ["год", "года", "лет"])}, `;
-    }
-    if (d > 0) {
-        text += `${d} ${utils.decl(d, ["день", "дня", "дней"])}, `;
-    }
-    if (h > 0) {
-        text += `${h} ${utils.decl(h, ["час", "часа", "часов"])}, `;
-    }
-    if (m > 0) {
-        text += `${m} ${utils.decl(m, ["минуту", "минуты", "минут"])}, `;
-    }
-    text += `${s} ${utils.decl(s, ["секунду", "секунды", "секунд"])}`;
+console.log('Имя до:', Person.firstName);
+Person.firstName = 'Сергей';
+console.log('Имя после:', Person.firstName);
 
-    return { seconds: s, minutes: m, hours: h, days: d, years: years, text: text };
+Person.day = 15;
+Person.month = 12;
+Person.year = 2022;
+let text = "";
+
+for (const key in Person) {
+  if (Object.prototype.hasOwnProperty.call(Person, key)) {
+    text += `${key}: ${Person[key]}\n`
+  }
 }
 
-const user = { name: "Vlad", age: 21 };
-user.checkLength = function(str) {
-    return str.length;
-}
+console.log(text);
 
-console.log(user.checkLength("Stringa"));
+const factGen = functions.factorialGenerator();
+factGen.next().value;
 
-function Power(base, exponent) {
-    this.base = base;
-    this.exponent = exponent;
-}
+console.log(factGen.next().value);
+console.log(factGen.next().value);
+console.log(factGen.next().value);
+console.log(factGen.next().value);
+console.log(factGen.next().value);
 
-Power.prototype.calculate = function() {
-    return Math.pow(this.base, this.exponent);
-}
-
-function calculatePower(base, exponent) {
-    const powerObj = new Power(base, exponent);
-    const result = powerObj.calculate();
-
-    return result;
-}
-
-console.log(calculatePower(8, 3));
-
-function createUrl(domain) {
-    return function(url) {
-        return `http://${url}.${domain}`;
-    }
-}
-
-const createMyUrl = createUrl("com");
-console.log(createMyUrl("google"));
+delete Person.lastName;
