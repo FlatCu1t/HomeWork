693fb99b08587008d475a88e9c5ad33de6fee416
FlatCult <ivlad6543@gmail.com>
2025-05-03 18:15:05 +0500

    36_jQuery и DOM. Анимация в jQuery

--- scripts/blocks.js
+++
@@ -1,9 +0,0 @@
-import { Functions } from "./functions.js";
-const functions = new Functions();
-
-$('#toggle').on('click', function() {
-    $('#sidebar').toggleClass('collapsed');
-
-    const isCollapsed = $('#sidebar').hasClass('collapsed');
-    $('#toggle .arrow').html(isCollapsed ? '&gt;' : '&lt;');
-});
--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,13 @@
 import { Functions } from "./functions.js";
 const functions = new Functions();
 
-const generateBtn = $("#generateBtn");
+const btn = $("#saveBtn");
+let colors = [];
 
-const charSets = {
-    digits: '0123456789',
-    upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    lower: 'abcdefghijklmnopqrstuvwxyz'
-};
-
-if (generateBtn) {
-    generateBtn.on("click", function() {
-        let length = parseInt($('#charLength').val(), 10);
-
-        if (isNaN(length) || length < 1) {
-            alert('Пожалуйста, введите корректную длину (число ≥ 1).');
-            return;
-        }
-
-        let availableChars = '';
-        if ($('#digits').is(':checked')) {
-          availableChars += charSets.digits;
-        }
-        if ($('#upper').is(':checked')) {
-          availableChars += charSets.upper;
-        }
-        if ($('#lower').is(':checked')) {
-          availableChars += charSets.lower;
-        }
-
-        if (availableChars.length === 0) {
-          alert('Пожалуйста, выберите хотя бы один тип символов.');
-          return;
-        }
-
-        let result = '';
-        for (let i = 0; i < length; i++) {
-          let idx = Math.floor(Math.random() * availableChars.length);
-          result += availableChars[idx];
-        }
-
-        $('#result').val(result);
-    });
-};
+if (btn) {
+  btn.on("click", () => {
+    if (functions.validateAndShow(colors)) {
+      functions.addColor();
+    }
+  });
+}
