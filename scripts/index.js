c995e69c6f6110ded590ab516ff53d4a258fb935
FlatCu1t <ivlad6543@gmail.com>
2025-05-17 13:25:37 +0500

    39_Работа с плагинами в jQuery

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,33 @@
 import { Functions } from "./functions.js";
 const functions = new Functions();
 
-$(function() {
-  $('.owl-carousel').owlCarousel({
-    items: 1,
-    loop: true,
-    nav: true,
-    dots: true,
-    navText: ['‹', '›'],
-    autoplay: false,
-    autoplayTimeout: 5000,
+$(document).ready(function() {
+  $('#phone').inputmask('+998 99 999-99-99');
+
+  $('#registrationForm').on('submit', function(e) {
+    e.preventDefault();
+    const name = $('#name').val().trim();
+    const phoneEl = $('#phone');
+    const phoneComplete = $('#phone').inputmask("isComplete");
+    const password = $('#password').val().trim();
+    const errors = [];
+
+    if (!name) {
+      errors.push('Поле "Имя" не заполнено.');
+    }
+    if (!phoneComplete) {
+      errors.push('Телефон введен неверно.');
+    }
+    if (!password) {
+      errors.push('Поле "Пароль" не заполнено.');
+    }
+
+    if (errors.length) {
+      errors.forEach(function(msg) {
+        toastr.error(msg, 'Ошибка');
+      });
+    } else {
+      toastr.success('Успешная регистрация!', 'OK');
+    }
   });
-  $('[data-fancybox="gallery"]').fancybox({ buttons: ['zoom','slideShow','close'], loop: true, protect: true });
 });
