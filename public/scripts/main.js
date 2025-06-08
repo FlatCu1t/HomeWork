836885569d67525259e19c340aff078bcbaa09a2
FlatCu1t <ivlad6543@gmail.com>
2025-06-08 15:18:38 +0500

    06_Шаблонизаторы. EJS

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import fs from "fs";
+import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
+import 'dotenv/config';
+const client = new MongoClient(process.env.dbURL || "");
+mongoose.connect(process.env.dbURL || "").catch((err) => console.error(err));
 const __mainDir = path.resolve();
 const __publicDir = path.join(__mainDir, "public");
 const app = express();
@@ -15,20 +20,63 @@ app.set('views', './public/views');
 app.get("/", (req, res) => {
     res.render("index");
 });
-app.get("/:fileID", async (req, res) => {
+app.get("/works", (req, res) => {
+    res.render("works", { work: null });
+});
+app.get("/works/addWork", (req, res) => {
+    res.render("addWork");
+});
+app.post("/works/addWork", async (req, res) => {
+    if (!req.body) {
+        res.sendStatus(500);
+        return;
+    }
+    const { workName, workDate, workImage } = req.body;
     try {
-        const fileID = req.params.fileID;
-        const data = await fs.promises.readFile(path.join(__publicDir, "data", `${fileID}.txt`), "utf-8");
-        if (data) {
-            res.status(200).send(data);
+        const response = await fetch("http://localhost:3000/getWorks");
+        if (response.ok && response.status == 200) {
+            let data = await response.json();
+            if (data) {
+                data = JSON.parse(data);
+                data.works.push({
+                    workID: data.works.length + 1,
+                    workName: workName,
+                    workDate: workDate,
+                    workImage: workImage
+                });
+                await fs.promises.writeFile(path.join(__publicDir, "data", "works.json"), JSON.stringify(data, null, 2), "utf-8");
+                res.sendStatus(200);
+            }
         }
     }
     catch (error) {
-        res.status(404).send(`Файла не существует, ваш запрос не доступен.\n${error}`);
+        res.sendStatus(500);
+    }
+});
+app.get("/works/:workID", async (req, res) => {
+    const workID = req.params.workID;
+    const response = await fetch("http://localhost:3000/getWorks");
+    if (response.ok && response.status == 200) {
+        let data = await response.json();
+        data = JSON.parse(data);
+        const finded = data.works.find((x) => x.workID == workID);
+        if (!finded) {
+            return res.render("404");
+        }
+        res.render("works", { work: finded });
+    }
+});
+app.get("/contacts", (req, res) => {
+    res.render("contacts", { user: null });
+});
+app.get("/getWorks", async (req, res) => {
+    const data = await fs.promises.readFile(path.join(__publicDir, "data", "works.json"), "utf-8");
+    if (data) {
+        res.json(data);
     }
 });
 app.use((req, res) => {
-    res.status(404).send("Файла не существует, ваш запрос не доступен.");
+    res.render("404");
 });
 app.listen(3000, () => {
     console.log("http://localhost:3000");
---
+++ src/addWork.ts
@@ -0,0 +1,17 @@
+const form = document.getElementById("addWorkForm");
+
+form?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const inputs: any = document.querySelectorAll(".addWorkInput");
+    if (inputs[0].value.length > 0 && inputs[1].value.length > 0) {
+        const response = await fetch("http://localhost:3000/works/addWork", {
+            method: "POST",
+            headers: { "content-type": "application/json; charset=utf-8" },
+            body: JSON.stringify({ workName: inputs[0].value, workDate: inputs[1].value, workImage: inputs[2].value || "/images/no-workImage.jpg" })
+        });
+
+        if (response.ok && response.status == 200) {
+            window.location.assign("/works");
+        }
+    }
+});
