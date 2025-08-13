c003d1de03fc07d4dc33785c6d42efa8cf330e3b
Tiltushkin <ivlad6543@gmail.com>
2025-08-13 22:15:04 +0500

    Добавлено отображение карточек + их добавление

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,15 +2,54 @@
   <div class="layout">
     <header>
       <h1>Shop</h1>
+      <button @click="clearStorage">Выход</button>
     </header>
     <main class="main">
-      <RouterView />
+      <section class="adminSection" v-if="admin">
+        <h2>Добавление карточки</h2>
+        <div class="inputs">
+          <input type="text" placeholder="cardTitle" v-model="cardTitle">
+          <input type="text" placeholder="cardDescription" v-model="cardDescription">
+          <input type="text" placeholder="cardIMG" v-model="cardIMG">
+          <button class="addButton" @click="addCard">Добавить</button>
+        </div>
+      </section>
+      <section>
+        <RouterView :cards="cards" :idCounter="idCounter" @removeCard="removeCard" />
+      </section>
     </main>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import admins from '../data/admins.json'
+import router from "../router/index.js";
+
+const cards = ref([])
+const idCounter = ref(0)
+const cardTitle = ref('')
+const cardDescription = ref('')
+const cardIMG = ref('')
+
+function addCard() {
+  if (cardTitle.value.length < 1 || cardDescription.value.length < 1) return;
+  cards.value.push({
+    cardID: idCounter.value++,
+    cardTitle: cardTitle.value,
+    cardDescription: cardDescription.value,
+    cardIMG: cardIMG.value
+  })
+}
+
+function removeCard(id) {
+  cards.value = cards.value.filter((card) => card.cardID !== id)
+}
+
+function clearStorage() {
+  localStorage.clear()
+  router.push('/reg')
+}
 
 const admin = admins.admins.find((element) => element === localStorage.getItem("user"));
 </script>
@@ -27,9 +66,71 @@ const admin = admins.admins.find((element) => element === localStorage.getItem("
     align-items: center;
     justify-content: center;
     text-align: center;
+    position: relative;
 
     h1 {
       font-size: 32px;
+    }
+
+    button {
+      position: absolute;
+      width: 120px;
+      height: 35px;
+      border: none;
+      border-radius: 8px;
+      font-size: 18px;
+      left: 80%;
+      transform: translateX(-80%);
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+
+  .adminSection {
+    width: 100%;
+    height: 30vh;
+    position: relative;
+
+    h2 {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: 10px;
+      font-size: 24px;
+    }
+
+    .inputs {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: 80px;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      gap: 30px;
+
+      input {
+        width: 250px;
+        height: 40px;
+        border: none;
+        border-radius: 10px;
+        padding-left: 10px;
+        font-size: 18px;
+      }
+
+      button {
+        width: 150px;
+        height: 40px;
+        border: none;
+        border-radius: 10px;
+        font-size: 20px;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home">
-    <div v-for="card in cards.cards" :key="card.cardID" class="card">
-      <p class="cardTitle">{{ card.cardTitle }}</p>
-      <p class="cardDesc">{{ card.cardDescription }}</p>
+    <div v-for="card in cards" :key="card.cardID" class="card">
+      <div class="content">
+        <img v-if="card.cardIMG" :src="card.cardIMG" loading="lazy" alt="card icon">
+        <p class="cardTitle">{{ card.cardTitle }}</p>
+        <p class="cardDesc">{{ card.cardDescription }}</p>
+        <button @click="removeCard(card.cardID)">Удалить</button>
+      </div>
     </div>
   </div>
+  <div class="empty"></div>
 </template>
 
 <script setup>
-import cards from '../data/cards.json'
+const props = defineProps({
+  cards: Array,
+  idCounter: Number
+})
+
+const emits = defineEmits(['removeCard'])
+
+function removeCard(id) {
+  emits('removeCard', id)
+}
+
 </script>
 
 <style>
@@ -29,25 +44,67 @@ import cards from '../data/cards.json'
 
   .card {
     position: relative;
-    width: 200px;
-    height: 300px;
+    min-width: 200px;
+    max-width: 600px;
+    height: auto;
     border-radius: 12px;
     background-color: #1c1c1c;
     transition: transform 0.35s ease-in-out;
 
+    .content {
+      width: inherit;
+      height: inherit;
+      padding: 15px;
+      box-sizing: border-box;
+      border-radius: inherit;
+
+      img {
+        max-width: 600px;
+        height: 100%;
+      }
+
+      button {
+        position: relative;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 120px;
+        height: 30px;
+        border: none;
+        border-radius: 10px;
+        margin-top: 20px;
+        box-shadow: 0 0 8px 2px rgba(0, 0, 0, 0.65);
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+
     .cardTitle, .cardDesc {
+      margin-top: 20px;
       font-size: 18px;
       align-self: center;
       justify-self: center;
+      text-align: center;
     }
 
     .cardTitle {
       font-weight: 600;
     }
 
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
     &:hover {
       transform: scale(1.1);
     }
   }
+}
+
+.empty {
+  width: 100%;
+  height: 300px;
 }
 </style>
